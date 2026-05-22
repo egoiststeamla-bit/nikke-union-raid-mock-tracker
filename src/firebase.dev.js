2ed9912be057a0c333b351f277e0033cb4f41bc8
Egoist <egoist.steamla@gmail.com>
2026-05-22 21:12:26 +0900

    Create firebase.dev.js

---
+++ src/firebase.dev.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmf1edyqT2Vbl9baIo9r9sruxilpEWfmw",
+  authDomain: "nikke-union-raid-mock-dev.firebaseapp.com",
+  projectId: "nikke-union-raid-mock-dev",
+  storageBucket: "nikke-union-raid-mock-dev.firebasestorage.app",
+  messagingSenderId: "528562170200",
+  appId: "1:528562170200:web:9aaf7db8731cb988c39d2d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
